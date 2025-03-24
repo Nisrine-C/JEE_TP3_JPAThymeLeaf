d2a49a72199d5028723b22ff6794006121e0e11e
Nisrine-C <nisrinechakir237@gmail.com>
2025-03-24 12:59:29 +0000

    search

--- src/main/java/ma/enset/jee_tp2/repository/PatientRepository.java
+++ src/main/java/ma/enset/jee_tp2/repository/PatientRepository.java
@@ -1,7 +1,11 @@
 package ma.enset.jee_tp2.repository;
 
 import ma.enset.jee_tp2.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
+
 }

1627c329f4bc180e14c06486708e053d1f2c0f26
Nisrine-C <nisrinechakir237@gmail.com>
2025-04-05 23:54:01 +0000

    working form for svaing a new patient

--- src/main/java/ma/enset/jee_tp2/web/PatientController.java
+++ src/main/java/ma/enset/jee_tp2/web/PatientController.java
@@ -8,20 +8,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@Controller@AllArgsConstructor
+@Controller
+@AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
     public String index(Model model,
                         @RequestParam(name="page",defaultValue = "0") int page,
-                        @RequestParam(name="size", defaultValue = "4") int size,
+                        @RequestParam(name="size", defaultValue = "10") int size,
                         @RequestParam(name="keyword",defaultValue = "") String keyword){
         Page<Patient> pagePatients = patientRepository.findByNomContains(keyword,PageRequest.of(page,size));
         model.addAttribute("listPatients",pagePatients.getContent());
@@ -42,6 +42,18 @@ public class PatientController {
     @GetMapping("/")
     public String home(){
         return "redirect:/index";
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatient(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping("/save")
+    public String save(Model model,@ModelAttribute Patient patient){
+        patientRepository.save(patient);
+        return "formPatients";
     }
 
 
